b5dc0d9472c912b039fde3317752cf013619ec84
Mayant <mayant.tipsgalwar@gmail.com>
2026-05-15 11:23:27 +0530

    Add C++ program to multiply two numbers
    This repository contains a basic C++ program to multiply two numbers using variables and display the result using cout.

---
+++ multiply.cpp
@@ -0,0 +1,15 @@
+//Added C++ multiply program
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 4;
+    int num2 = 6;
+    int multiply = num1 * num2;
+
+    cout << "Num1 is: " << num1 << "\n";
+    cout << "Num2 is: " << num2 << "\n";
+    cout << "Multiply is: " << multiply << "\n";
+
+    return 0;
+}
