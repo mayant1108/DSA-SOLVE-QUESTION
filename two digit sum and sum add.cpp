13c71074de66f3a61f97a0714b81b63a2b071647
Mayant <mayant.tipsgalwar@gmail.com>
2026-05-17 21:35:47 +0530

    two digit sum and add sum
    Declares three integer variables x, y, and sum
    Takes two numbers as input from the user
    Adds the two numbers
    Displays the result using cout

---
+++ two digit sum and sum add.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+int x,y,sum;
+cout << "Enter first Number";
+cin >> x;
+cout << "Enter second Number";
+cin >> y;
+sum = x+y;
+cout << "Sum =" << sum;
+}
